0fa72a90aedf3d634825ef5c5887c2a82cbf9112
Vikas Parashar <svnitvikas@gmail.com>
2017-03-10 11:30:56 +0530

    :tada: youtube iframe opens in modal now and play button is alive

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
-$(document).ready(function() {
-	$('#play-video').on('click', function(ev) {
-		this.style.display = 'none';
-		thevid = document.getElementById('video');
-		thevid.style.display = 'block';
-		$("#video")[0].src += "?autoplay=1";
+$(document).ready(function () {
+	var url = $("#video")[0].src
+	$('#play-video').on('click', function (ev) {
+
+		$("#video")[0].src = url + "?autoplay=1";
 		ev.preventDefault();
 
 	});
+	$("#play-modal").on('hidden.bs.modal', function (e) {
+		$("#play-modal iframe").attr("src", url);
+	});
+	$('.video-container').hover(function (){
+		$('.play-button').toggleClass('scale')
+	});
+
 });
 var about = document.querySelector('.about-container');
 var video = document.querySelector('.video-container');
-var play_video = document.getElementById('play-video');
+var play_video = document.getElementById('play-video-bg');
 var yt = document.querySelector('iframe');
 var aboutWidth = about.getBoundingClientRect().width;
 var aboutHeight = about.getBoundingClientRect().height;
