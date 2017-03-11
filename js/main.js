3e1b5ff2f35879d8ddb7b1798cb18108533508b0
akashnimare <svnitakash@gmail.com>
2017-03-12 03:16:31 +0530

    :tada: added ajax support

--- js/main.js
+++ js/main.js
@@ -37,25 +37,39 @@ if (windowWidth > 992) {
 }
 console.log("windowWidth:", windowWidth)
 console.log("windowHeight:", windowHeight)
-var frm = $('#feedback-form');
-    frm.submit(function (ev) {
-      // $('.fail span').toggleClass('hide-msg')
-      $('.success span').toggleClass('hide-msg')
-      $.ajax({
-        type: frm.attr('method'),
-        // type: 'GET',
-        url: frm.attr('action'),
-        // url: '/',
-        data: {feedback: $('#feedback-form textarea').val()},
-        success: function (data) {
-          $('.fail span').addClass('hide-msg')
-          $('.success span').removeClass('hide-msg')
-        },
-        error: function () {
-          $('.success span').addClass('hide-msg')
-          $('.fail span').removeClass('hide-msg')
-        }
-      });
 
-      ev.preventDefault();
-    })
+
+
+$(document).ready( function () {
+    var $form = $('form');
+    if ( $form.length > 0 ) {
+        $('form input[type="submit"]').bind('click', function ( event ) {
+            if ( event ) event.preventDefault();
+			register($form);
+        });
+    }
+});
+
+function register($form) {
+    $.ajax({
+        type: $form.attr('method'),
+        url: $form.attr('action'),
+        data: $form.serialize(),
+        cache       : false,
+        dataType    : 'json',
+        contentType: "application/json; charset=utf-8",
+        error       : function(err) { alert("Could not connect to the registration server. Please try again later."); },
+        success     : function(data) {
+            if (data.result != "success") {
+                // Something went wrong, do something to notify the user.
+				console.log(data.msg);
+				$( ".error" ).html( "<p>" + data.msg.substring(4) + "</p>" );
+				$('.success').hide();
+            } else {
+				$('.error').hide();
+				$( ".success" ).html( "<p>Thank you for subscribing</p>" );
+            }
+        }
+    });
+}
+
