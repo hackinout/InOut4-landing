5d32216e7f82759b8dc3388af487af263e3d9a59
akashnimare <svnitakash@gmail.com>
2017-03-26 02:07:27 +0530

    add ajax processing request status

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var windowWidth = Math.max(document.documentElement.clientWidth, window.innerWid
 var windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
 window.onload = defaultaboutHeight();
-window.addEventListener('resize', function(event){
+window.addEventListener('resize', function (event) {
     setaboutHeight();
 });
 
@@ -30,50 +30,52 @@ console.log("windowWidth:", windowWidth)
 console.log("windowHeight:", windowHeight)
 
 $(document).ready(function () {
-	var url = $("#video")[0].src
-	$('#play-video').on('click', function (ev) {
-		$("#video")[0].src = url + "?autoplay=1";
-		ev.preventDefault();
-	});
-	$("#play-modal").on('hidden.bs.modal', function (e) {
-		$("#play-modal iframe").attr("src", url);
-	});
-	$('.video-container').hover(function (){
-		$('.play-button').toggleClass('scale')
-	})
+    var url = $("#video")[0].src
+    $('#play-video').on('click', function (ev) {
+        $("#video")[0].src = url + "?autoplay=1";
+        ev.preventDefault();
+    });
+    $("#play-modal").on('hidden.bs.modal', function (e) {
+        $("#play-modal iframe").attr("src", url);
+    });
+    $('.video-container').hover(function () {
+        $('.play-button').toggleClass('scale')
+    })
 })
 
-$(document).ready( function () {
+$(document).ready(function () {
     var $form = $('form');
-    if ( $form.length > 0 ) {
-        $('form input[type="submit"]').bind('click', function ( event ) {
-            if ( event ) event.preventDefault();
-			register($form);
+    if ($form.length > 0) {
+        $('form input[type="submit"]').bind('click', function (event) {
+            if (event) event.preventDefault();
+            register($form);
         });
     }
 });
 
 function register($form) {
+    $("#mc-embedded-subscribe").val("checking...")
     $.ajax({
         type: $form.attr('method'),
         url: $form.attr('action'),
         data: $form.serialize(),
-        cache       : false,
-        dataType    : 'json',
+        cache: false,
+        dataType: 'json',
         contentType: "application/json; charset=utf-8",
-        error       : function(err) { alert("Could not connect to the registration server. Please try again later."); },
-        success     : function(data) {
+        error: function (err) { alert("Could not connect to the registration server. Please try again later."); },
+        success: function (data) {
+            $("#mc-embedded-subscribe").val("subscribe")
             if (data.result != "success") {
                 // Something went wrong, do something to notify the user.
-				console.log(data.msg);
-                $("#mce-EMAIL").css("borderColor","#ff8282")
-                $("#subscribe-result").css("color","#ff8282")
+                console.log(data.msg);
+                $("#mce-EMAIL").css("borderColor", "#ff8282")
+                $("#subscribe-result").css("color", "#ff8282")
                 $("#subscribe-result").html("<p>" + data.msg.substring(4) + "</p>");
             } else {
                 // Yeahhhh Success
-				console.log(data.msg);
-                $("#mce-EMAIL").css("borderColor","#ffffff")
-                $("#subscribe-result").css("color","#ffffff")
+                console.log(data.msg);
+                $("#mce-EMAIL").css("borderColor", "#ffffff")
+                $("#subscribe-result").css("color", "#ffffff")
                 $("#subscribe-result").html("<p>Thank you for subscribing. We have sent you a confirmation email.</p>");
                 $("#mce-EMAIL").val("");
             }
