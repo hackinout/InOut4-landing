e388a9c7368c49fae9e4c9de9ff6c288b23e27e0
Vikas Parashar <svnitvikas@gmail.com>
2017-03-29 00:56:12 +0530

    about section redesigned using flex

--- source/js/main.js
+++ source/js/main.js
@@ -1,84 +1,76 @@
-var about = document.querySelector('.about-container');
-var video = document.querySelector('.video-container');
-var play_video = document.getElementById('play-video-bg');
-var aboutWidth = about.getBoundingClientRect().width;
-var aboutHeight = about.getBoundingClientRect().height;
-var play_videoHeight = play_video.getBoundingClientRect().height;
-var windowWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-var windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+var windowWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+var av = document.querySelector('.about-video')
 
-window.onload = defaultaboutHeight();
-window.addEventListener('resize', function (event) {
-    setaboutHeight();
-});
+if (windowWidth > 992) {
+  av.classList.add('flex')
+  console.log(av.classList)
+} else {
+  av.classList.remove('flex')
+}
 
-function setaboutHeight() {
-    var resizeplay_videoHeight = play_video.getBoundingClientRect().height;
-    var resizewindowWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    if (resizewindowWidth > 992) {
-        about.style.height = resizeplay_videoHeight + 'px';
+function setaboutHeight (e) {
+  var w = e.target.innerWidth
+  if (w > 992) {
+      av.classList.add('flex')
+    } else {
+      av.classList.remove('flex')
     }
 }
 
-function defaultaboutHeight() {
-    if (windowWidth > 992) {
-        about.style.height = play_videoHeight + 'px';
-    }
-};
-
-console.log("windowWidth:", windowWidth)
-console.log("windowHeight:", windowHeight)
+window.addEventListener('resize', setaboutHeight, true)
 
 $(document).ready(function () {
-    var url = 'https://www.youtube.com/embed/SzmioR4Dj8E'
-    $('#play-video').on('click', function (ev) {
-        $("#video")[0].src = url + "?autoplay=1";
-        ev.preventDefault();
-    });
-    $("#play-modal").on('hidden.bs.modal', function (e) {
-        $("#play-modal iframe").attr("src", url);
-    });
-    $('.video-container').hover(function () {
-        $('.play-button').toggleClass('scale')
+  var url = 'https://www.youtube.com/embed/SzmioR4Dj8E'
+  $('#play-video').on('click', function (ev) {
+      $('#video')[0].src = url + '?autoplay=1'
+      ev.preventDefault()
+      console.log('played')
+    })
+  $('#play-modal').on('hidden.bs.modal', function (e) {
+      $('#play-modal iframe').attr('src', url)
+      console.log('stopped')
+    })
+  $('.video-container').hover(function () {
+      $('.play-button').toggleClass('scale')
     })
 })
 
 $(document).ready(function () {
-    var $form = $('form');
-    if ($form.length > 0) {
-        $('form input[type="submit"]').bind('click', function (event) {
-            if (event) event.preventDefault();
-            register($form);
-        });
+  var $form = $('form')
+  if ($form.length > 0) {
+      $('form input[type="submit"]').bind('click', function (event) {
+          if (event) event.preventDefault()
+          register($form)
+        })
     }
-});
+})
 
-function register($form) {
-    $("#mc-embedded-subscribe").val("checking...")
-    $.ajax({
-        type: $form.attr('method'),
-        url: $form.attr('action'),
-        data: $form.serialize(),
-        cache: false,
-        dataType: 'json',
-        contentType: "application/json; charset=utf-8",
-        error: function (err) { alert("Could not connect to the registration server. Please try again later."); },
-        success: function (data) {
-            $("#mc-embedded-subscribe").val("subscribe")
-            if (data.result != "success") {
+function register ($form) {
+  $('#mc-embedded-subscribe').val('checking...')
+  $.ajax({
+      type: $form.attr('method'),
+      url: $form.attr('action'),
+      data: $form.serialize(),
+      cache: false,
+      dataType: 'json',
+      contentType: 'application/json; charset=utf-8',
+      error: function (err) { alert('Could not connect to the registration server. Please try again later.') },
+      success: function (data) {
+          $('#mc-embedded-subscribe').val('subscribe')
+          if (data.result != 'success') {
                 // Something went wrong, do something to notify the user.
-                console.log(data.msg);
-                $("#mce-EMAIL").css("borderColor", "#ff8282")
-                $("#subscribe-result").css("color", "#ff8282")
-                $("#subscribe-result").html("<p>" + data.msg.substring(4) + "</p>");
+              console.log(data.msg)
+              $('#mce-EMAIL').css('borderColor', '#ff8282')
+              $('#subscribe-result').css('color', '#ff8282')
+              $('#subscribe-result').html('<p>' + data.msg.substring(4) + '</p>')
             } else {
                 // Yeahhhh Success
-                console.log(data.msg);
-                $("#mce-EMAIL").css("borderColor", "#ffffff")
-                $("#subscribe-result").css("color", "#ffffff")
-                $("#subscribe-result").html("<p>Thank you for subscribing. We have sent you a confirmation email.</p>");
-                $("#mce-EMAIL").val("");
+              console.log(data.msg)
+              $('#mce-EMAIL').css('borderColor', '#ffffff')
+              $('#subscribe-result').css('color', '#ffffff')
+              $('#subscribe-result').html('<p>Thank you for subscribing. We have sent you a confirmation email.</p>')
+              $('#mce-EMAIL').val('')
             }
         }
-    });
+    })
 };
