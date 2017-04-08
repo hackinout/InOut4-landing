0e386095e9f41d493bc065d5b8fc5bdf3f59e1a6
akashnimare <svnitakash@gmail.com>
2017-04-08 23:51:59 +0530

    :bug: fixed newsletter response error

--- source/js/main.js
+++ source/js/main.js
@@ -8,13 +8,13 @@ if (windowWidth > 992) {
   av.classList.remove('flex')
 }
 
-function setaboutHeight (e) {
+function setaboutHeight(e) {
   var w = e.target.innerWidth
   if (w > 992) {
-      av.classList.add('flex')
-    } else {
-      av.classList.remove('flex')
-    }
+    av.classList.add('flex')
+  } else {
+    av.classList.remove('flex')
+  }
 }
 
 window.addEventListener('resize', setaboutHeight, true)
@@ -22,55 +22,59 @@ window.addEventListener('resize', setaboutHeight, true)
 $(document).ready(function () {
   var url = 'https://www.youtube.com/embed/SzmioR4Dj8E'
   $('#play-video').on('click', function (ev) {
-      $('#video')[0].src = url + '?autoplay=1'
-      ev.preventDefault()
-      console.log('played')
-    })
+    $('#video')[0].src = url + '?autoplay=1'
+    ev.preventDefault()
+    console.log('played')
+  })
   $('#play-modal').on('hidden.bs.modal', function (e) {
-      $('#play-modal iframe').attr('src', url)
-      console.log('stopped')
-    })
+    $('#play-modal iframe').attr('src', url)
+    console.log('stopped')
+  })
   $('.video-container').hover(function () {
-      $('.play-button').toggleClass('scale')
-    })
+    $('.play-button').toggleClass('scale')
+  })
 })
 
 $(document).ready(function () {
   var $form = $('form')
   if ($form.length > 0) {
-      $('form input[type="submit"]').bind('click', function (event) {
-          if (event) event.preventDefault()
-          register($form)
-        })
-    }
+    $('form input[type="submit"]').bind('click', function (event) {
+      if (event) event.preventDefault()
+      register($form)
+    })
+  }
 })
 
-function register ($form) {
+function register($form) {
   $('#mc-embedded-subscribe').val('checking...')
   $.ajax({
-      type: $form.attr('method'),
-      url: $form.attr('action'),
-      data: $form.serialize(),
-      cache: false,
-      dataType: 'json',
-      contentType: 'application/json; charset=utf-8',
-      error: function (err) { alert('Could not connect to the registration server. Please try again later.') },
-      success: function (data) {
-          $('#mc-embedded-subscribe').val('subscribe')
-          if (data.result != 'success') {
-                // Something went wrong, do something to notify the user.
-              console.log(data.msg)
-              $('#mce-EMAIL').css('borderColor', '#ff8282')
-              $('#subscribe-result').css('color', '#ff8282')
-              $('#subscribe-result').html('<p>' + data.msg.substring(4) + '</p>')
-            } else {
-                // Yeahhhh Success
-              console.log(data.msg)
-              $('#mce-EMAIL').css('borderColor', '#ffffff')
-              $('#subscribe-result').css('color', '#ffffff')
-              $('#subscribe-result').html('<p>Thank you for subscribing. We have sent you a confirmation email.</p>')
-              $('#mce-EMAIL').val('')
-            }
+    type: $form.attr('method'),
+    url: $form.attr('action'),
+    data: $form.serialize(),
+    cache: false,
+    dataType: 'json',
+    contentType: 'application/json; charset=utf-8',
+    error: function (err) { alert('Could not connect to the registration server. Please try again later.') },
+    success: function (data) {
+      $('#mc-embedded-subscribe').val('subscribe')
+      if (data.result != 'success') {
+        // Something went wrong, do something to notify the user.
+        console.log(data.msg)
+        $('#mce-EMAIL').css('borderColor', '#ff8282')
+        $('#subscribe-result').css('color', '#ff8282')
+        if (data.msg.indexOf("already") >= 0) {
+          $('#subscribe-result').html('<p>' + data.msg + '</p>')
+        } else {
+          $('#subscribe-result').html('<p>' + data.msg.substring(4) + '</p>')
         }
-    })
+      } else {
+        // Yeahhhh Success
+        console.log(data.msg)
+        $('#mce-EMAIL').css('borderColor', '#ffffff')
+        $('#subscribe-result').css('color', '#ffffff')
+        $('#subscribe-result').html('<p>Thank you for subscribing. We have sent you a confirmation email.</p>')
+        $('#mce-EMAIL').val('')
+      }
+    }
+  })
 };
